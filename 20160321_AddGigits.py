a8d127b9edeeac4bc0a3755805f107c21195bc68
Rong <yangr.rong@gmail.com>
2016-03-23 14:01:41 +0800

    Update 20160321_AddGigits.py
    Final version with 3 solutions.

--- 20160321_AddGigits.py
+++ 20160321_AddGigits.py
@@ -2,26 +2,12 @@
 # -*- coding: utf-8 -*-
 
 '''
-Nim Game https://leetcode.com/problems/nim-game/
-You are playing the following Nim Game with your friend: There is a heap of stones on the table, each time one of you take turns to remove 1 to 3 stones. The one who removes the last stone will be the winner. You will take the first turn to remove the stones.  Both of you are very clever and have optimal strategies for the game. Write a function to determine whether you can win the game given the number of stones in the heap.  For example, if there are 4 stones in the heap, then you will never win the game: no matter 1, 2, or 3 stones you remove, the last stone will always be removed by your friend.  1次可以移除1到3块石头，你先移，移最后一块石头者为优胜者
-
-解法：找到石头的数目和输赢的联系；如石头数是4的整数倍，先移者必输；返回值为bool （true or false）
-
-解法1:
-        if n%4 == 0:
-                    return False
-                            else:
-                                        return True
-
-                                        解法2: return bool(n%4)
-                                        解法3: return False if n%4 ==0 else True (三元符的替代: pythonic写法：http://wuzhiwei.net/be_pythonic/）
+Add Digits https://leetcode.com/problems/add-digits/
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.  For example:
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
 '''
 
-#Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.  For example:
-#Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
-
 # Solution1: loop
-
 def turnNumAsList(num):
     tmpStr = str(num)
     i = 0
@@ -38,8 +24,8 @@ def addDigitLoop(num):
         tmpNum = sum(turnNumAsList(tmpNum))
 
     return tmpNum
-# Solution: recursive
 
+# Solution2: recursive
 def addDigitRecursive(num):
     if num <= 9:
         return num
@@ -63,7 +49,6 @@ print "138:", addDigitLoop(138)
 print "8:", addDigitRecursive(8)
 print "38:", addDigitRecursive(38)
 print "138:", addDigitRecursive(138)
-
 
 print "8:", addDigit(8)
 print "38:", addDigit(38)
