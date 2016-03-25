d4b55ab1fb09e5d6807e0bb2baba5f435ea6ffc8
Yang Rong <yangr.rong@gmail.com>
2016-03-26 00:13:04 +0800

    3 solutions

--- 20160325_UglyNum.py
+++ 20160325_UglyNum.py
@@ -9,27 +9,90 @@ For example, 6, 8 are ugly while 14 is not ugly since it includes another prime 
 Note that 1 is typically treated as an ugly number. 
 '''
 
+'''
+Solution 1: 递归解法
+'''
 class Solution(object):
     def isUgly(self, num):
         """
         :type num: int
         :rtype: bool
         """
+		#Base situation 
         if num <= 0:
             return False
-        if num == 1 or num == 2 or num == 3 or num == 5:
+        if num == 1 or num == 2 or num == 3 or num == 5:  #可以写成 num in (1,2,3,5)
             return True
+
+		#Recursion
         if num % 2 == 0:
             return Solution.isUgly(self, num / 2)
-        if num % 3 == 0:
+        elif num % 3 == 0:
             return Solution.isUgly(self, num / 3)        
-        if num % 5 == 0:
+        elif num % 5 == 0:
             return Solution.isUgly(self, num / 5)
-        return False
-        
+        else:
+			return False
+
+'''
+Solution 2: 递归解法
+'''        
+class Solution2(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        # Base situation
+        if num < 1:
+            return False
+        if num in (1,2,3,5):
+            return True
+        # Recursion
+        q5, r5 = divmod(num,5)  #返回商与余数的元组  http://www.ahlinux.com/python/9668.html
+        if r5 == 0:
+            return self.isUgly(q5)
+        else:
+            q3, r3 = divmod(num,3)
+            if r3 == 0:
+                return self.isUgly(q3)
+            else:
+                q2, r2 = divmod(num , 2)
+                if r2 == 0:
+                    return self.isUgly(q2)
+                else:
+                    return False
+
+
+'''
+Solution 3: 循环
+'''
+class Solution3(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        # Base situation
+        if num < 1:
+            return False
+        if num in (1,2,3,5):
+            return True
+
+        while (num%2 == 0):
+			num = num / 2
+        while num % 3 == 0:
+			num = num / 3
+        while num % 5 == 0:
+			num = num / 5
+		
+        if num == 1:
+			return True
+        else:
+			return False
 
 ######### unit test cases:
-mySolution = Solution()
+mySolution = Solution3()
 
 import unittest
 
